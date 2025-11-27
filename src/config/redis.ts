c68daac15728e89e32513fb367872fea0d9a303d
niraj12chaudhary <shivamkr7822@gmail.com>
2025-11-27 13:10:08 +0530

    chore: update redis config

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,6 +1,9 @@
 import { ConnectionOptions } from "bullmq";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const redisConnection: ConnectionOptions = {
-  host: "localhost",
-  port: 6379,
+  host: process.env.REDIS_HOST || "localhost",
+  port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
 };
